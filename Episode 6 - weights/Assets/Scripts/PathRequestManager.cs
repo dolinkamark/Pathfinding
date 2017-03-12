2fb20d4454b05aaeda323522dc15bd97891add01
Márk Gergely Dolinka <dolinkamark@hotmail.com>
2017-03-12 14:56:28 +0100

    Remove unity generated Library folder from git in Episode 06 - weights.

--- Episode 6 - weights/Assets/Scripts/PathRequestManager.cs
+++ Episode 6 - weights/Assets/Scripts/PathRequestManager.cs
@@ -3,51 +3,58 @@ using System.Collections;
 using System.Collections.Generic;
 using System;
 
-public class PathRequestManager : MonoBehaviour {
+public class PathRequestManager : MonoBehaviour
+{
+    Queue<PathRequest> pathRequestQueue = new Queue<PathRequest>();
+    PathRequest currentPathRequest;
+     
+    static PathRequestManager instance;
+    Pathfinding pathfinding;
 
-	Queue<PathRequest> pathRequestQueue = new Queue<PathRequest>();
-	PathRequest currentPathRequest;
+    bool isProcessingPath;
 
-	static PathRequestManager instance;
-	Pathfinding pathfinding;
+    void Awake()
+    {
+        instance = this;
+        pathfinding = GetComponent<Pathfinding>();
+    }
 
-	bool isProcessingPath;
+    public static void RequestPath(Vector3 pathStart, Vector3 pathEnd, Action<Vector3[], bool> callback)
+    {
+        PathRequest newRequest = new PathRequest(pathStart, pathEnd, callback);
+        instance.pathRequestQueue.Enqueue(newRequest);
+        instance.TryProcessNext();
+    }
 
-	void Awake() {
-		instance = this;
-		pathfinding = GetComponent<Pathfinding>();
-	}
+    void TryProcessNext()
+    {
+        if (!isProcessingPath && pathRequestQueue.Count > 0)
+        {
+            currentPathRequest = pathRequestQueue.Dequeue();
+            isProcessingPath = true;
+            pathfinding.StartFindPath(currentPathRequest.pathStart, currentPathRequest.pathEnd);
+        }
+    }
 
-	public static void RequestPath(Vector3 pathStart, Vector3 pathEnd, Action<Vector3[], bool> callback) {
-		PathRequest newRequest = new PathRequest(pathStart,pathEnd,callback);
-		instance.pathRequestQueue.Enqueue(newRequest);
-		instance.TryProcessNext();
-	}
+    public void FinishedProcessingPath(Vector3[] path, bool success)
+    {
+        currentPathRequest.callback(path, success);
+        isProcessingPath = false;
+        TryProcessNext();
+    }
 
-	void TryProcessNext() {
-		if (!isProcessingPath && pathRequestQueue.Count > 0) {
-			currentPathRequest = pathRequestQueue.Dequeue();
-			isProcessingPath = true;
-			pathfinding.StartFindPath(currentPathRequest.pathStart, currentPathRequest.pathEnd);
-		}
-	}
+    struct PathRequest
+    {
+        public Vector3 pathStart;
+        public Vector3 pathEnd;
+        public Action<Vector3[], bool> callback;
 
-	public void FinishedProcessingPath(Vector3[] path, bool success) {
-		currentPathRequest.callback(path,success);
-		isProcessingPath = false;
-		TryProcessNext();
-	}
+        public PathRequest(Vector3 _start, Vector3 _end, Action<Vector3[], bool> _callback)
+        {
+            pathStart = _start;
+            pathEnd = _end;
+            callback = _callback;
+        }
 
-	struct PathRequest {
-		public Vector3 pathStart;
-		public Vector3 pathEnd;
-		public Action<Vector3[], bool> callback;
-
-		public PathRequest(Vector3 _start, Vector3 _end, Action<Vector3[], bool> _callback) {
-			pathStart = _start;
-			pathEnd = _end;
-			callback = _callback;
-		}
-
-	}
+    }
 }
